2fedd7b10a10b7c392ec05f329c656ea8348c586
Jared Radin <jared@r4d1n.net>
2025-12-10 10:43:37 -0800

    swap completion plugin

--- lua/configs/lsp.lua
+++ lua/configs/lsp.lua
@@ -1,4 +1,4 @@
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local capabilities = require("blink.cmp").get_lsp_capabilities()
 vim.lsp.config("*", { capabilities = capabilities })
 
 vim.lsp.enable("eslint")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -50,20 +50,4 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
 		opts = {},
 	},
-	"hrsh7th/nvim-cmp",
-	"hrsh7th/cmp-nvim-lsp",
-	"hrsh7th/cmp-buffer",
-	"hrsh7th/cmp-path",
-	"hrsh7th/cmp-cmdline",
-	{ "petertriho/cmp-git", dependencies = "nvim-lua/plenary.nvim" },
-	"hrsh7th/cmp-nvim-lsp-signature-help",
-	{
-		"L3MON4D3/LuaSnip",
-		-- follow latest release.
-		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-		-- install jsregexp (optional!).
-		build = "make install_jsregexp",
-	},
-	"saadparwaiz1/cmp_luasnip",
-	"rafamadriz/friendly-snippets",
 }
