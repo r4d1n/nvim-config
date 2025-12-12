52003561e72d2609918b48acf79bee7c47516df0
Jared Radin <jared@r4d1n.net>
2025-12-11 17:50:24 -0800

    Revert "remove unnecessary dependency"
    This reverts commit 8c1862fe01ce0e610add7089cc7e83ecfb1f931a.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -38,7 +38,7 @@ return {
 	},
 	{
 		"pmizio/typescript-tools.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
 		opts = {},
 	},
 }
