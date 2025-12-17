c82b5ed507e1c847984d1a68c98d0e21637d7624
Jared Radin <jared@r4d1n.net>
2025-12-17 12:07:46 -0800

    turn off blink preselect option

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -43,7 +43,18 @@ return {
 			nerd_font_variant = "mono",
 		},
 
-		completion = { documentation = { auto_show = true, auto_show_delay_ms = 250 } },
+		completion = {
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 250,
+			},
+			list = {
+				selection = {
+					preselect = false,
+					auto_insert = true,
+				},
+			},
+		},
 
 		-- Default list of enabled providers defined so that you can extend it
 		-- elsewhere in your config, without redefining it, due to `opts_extend`
