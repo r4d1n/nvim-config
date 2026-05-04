b6e18e201140c20f2ab7844babbc02e4c250e854
Jared Radin <jared@r4d1n.net>
2026-05-04 11:26:00 -0700

    nvim 0.12 upgrade changes

--- lua/configs/lsp.lua
+++ lua/configs/lsp.lua
@@ -11,15 +11,15 @@ vim.lsp.enable("html")
 
 require("typescript-tools").setup({})
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+vim.diagnostic.config({
 	underline = true,
 	virtual_text = false,
 })
 
 vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>do", "<cmd>lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>dp", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>dn", "<cmd>lua vim.diagnostic.goto_next()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>dp", function() vim.diagnostic.jump({ count = -1 }) end, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>dn", function() vim.diagnostic.jump({ count = 1 }) end, { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>dd", "<cmd>Telescope diagnostics<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "rn", vim.lsp.buf.rename, {})
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
